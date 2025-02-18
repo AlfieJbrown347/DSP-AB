826b3b5a3f4deb32cceb4e5cc6ad2d1e898b87e0
alfie brown <alfredobrown@alfies-MacBook-Air.local>
2025-02-18 13:54:53 +0000

    added milestones

--- test.m
+++ test.m
@@ -1,1 +1,13 @@
-%test
+%audio filename must in same directory of this m file
+Filename = 'piano_middle_C.wav';
+% First read wav file into memory
+[Sig, Fs]= audioread(Filename);
+%Sig stores raw audio data in column;
+%Fs sampling frequency
+
+duration = length(sig)/Fs;
+
+Ts=1/fs;
+
+time = 0:Ts:duration-Ts;
+Time = time';
